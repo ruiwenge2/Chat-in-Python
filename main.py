c1cc57da1a81d09912c98d2b3a470a6d64d2c06e
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2021-10-16 01:33:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request, redirect, session
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
+app = Flask("app", static_url_path="")
+app.config["SECRET_KEY"] = os.getenv("secret")
+
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+  return render_template("index.html", loggedin=("username" in session), session=session)
+
+@app.route("/setusername", methods=["POST"])
+def setusername():
+  if "username" in request.form:
+    session["username"] = request.form["username"]
+  return redirect("/")
+
+@socketio.on("joined")
+def joined():
+  join_room("main")
+
+@socketio.on("message")
+def chat_message(user, room, message):
+  emit("message", {"user":user, "message":message}, room=room)
+
+@socketio.on("changeroom")
+def changeroom(room):
+  join_room(room)
+  if room == "main":
+    words = "You are in the main room"
+  else:
+    words = "You are in the room " + room
+  emit("room", words)
+
+socketio.run(app, host="0.0.0.0", port=8080)
